6830b01a2aadfddef8e9febd3c664b0b5c6838db
yamolean <moririnmanro9608@gmail.com>
2019-11-16 01:25:59 +0900

    delete sample urlsession

--- IwannabeYumemi/Api.swift
+++ IwannabeYumemi/Api.swift
@@ -43,36 +43,3 @@ struct Api {
         
     }
 }
-
-
-//func post(url urlString: String, parameters: [String: Any]) {
-//        let url = URL(string: urlString)
-//        var request = URLRequest(url: url!)
-//        request.httpMethod = "POST"
-//
-//        let parametersString: String = parameters.enumerated().reduce("?") { (input, tuple) -> String in
-//            switch tuple.element.value {
-//            case let int as Int: return input + tuple.element.key + "=" + String(int) + (parameters.count - 1 > tuple.offset ? "&" : "")
-//            case let string as String: return input + tuple.element.key + "=" + string + (parameters.count - 1 > tuple.offset ? "&" : "")
-//            default: return input
-//            }
-//        }
-//
-//        request.httpBody = parametersString.data(using: String.Encoding.utf8)
-//        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-//            if let data = data, let response = response {
-//                print(response)
-//                do {
-//                    let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
-//                    print(json)
-//                } catch {
-//                    print("Serialize Error")
-//                }
-//            } else {
-//                print(error ?? "Error")
-//            }
-//        }
-//        task.resume()
-//    }
-//
-//}
